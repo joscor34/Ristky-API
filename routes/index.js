337beca2e6fc9974b895a37b198efffba9e3de77
Jose Eduardo Esteva Cordova <zbrm34@gmail.com>
2020-03-12 18:03:36 -0600

    Rutas para crear y ver todos los productos añadidas

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const productCtrl = require('../controllers/productCtlr')
 const api = express.Router()
 
 api.get('/1', (req, res) => {
   res.send('Hola we').status(200)
 })
+
+api.post('/productos/crear', productCtrl.crearProducto)
+api.get('/productos/all', productCtrl.obtenerProductos)
 
 module.exports = api
